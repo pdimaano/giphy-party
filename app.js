369e4294523fb4fb9c3a1ff9af717f427da7fa55
Philip Dimaano <pdimaano7@gmail.com>
2022-10-04 16:20:08 -0700

    Code for successful response from giphy website

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+$("form").on("submit", searchGiphy);
+
+async function searchGiphy(evt) {
+  evt.preventDefault();
+  let giphyValue = document.querySelector("input").value;
+  let response = await axios.get('http://api.giphy.com/v1/gifs/search',
+  {params: {q: giphyValue, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'}})
+  console.log(response)
+}
