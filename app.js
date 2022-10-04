cefad75d2e128e3cecd28e05e0c0a5ef97b94ae9
Philip Dimaano <pdimaano7@gmail.com>
2022-10-04 16:49:22 -0700

    Started functions for appending gif to container

--- app.js
+++ app.js
@@ -9,5 +9,18 @@ async function searchGiphy(evt) {
   let giphyValue = document.querySelector("input").value;
   let response = await axios.get('http://api.giphy.com/v1/gifs/search',
   {params: {q: giphyValue, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'}})
-  console.log(response)
+  let randomIndex = generateRandomNumber(response.data.data.length);
+  console.log(response.data.data)
+}
+
+function generateRandomNumber(arrayLength) {
+  let randomNum = Math.floor(Math.random() * arrayLength);
+  return randomNum;
+}
+
+function appendGiphy(urlString) {
+  let giphy = document.createElement("img");
+  giphy.src = urlString;
+  let container = document.getElementById("giphy-container");
+  container.append(giphy);
 }
